bceff109ece8fca16b9c173b6dc0b80745692d29
Martin Tamajka <martin.tamajka@gmail.com>
2017-11-26 21:35:14 +0100

    Initial

---
+++ ReduxExtension/Actions/AnyAction.cs
@@ -0,0 +1,10 @@
+﻿namespace Redux.Actions
+{
+    /// <summary>
+    /// If this type is used in a Reducer or an Effect, 
+    /// it will be triggered if any action is passed to the Store.
+    /// </summary>
+    public class AnyAction : IAction
+    {
+    }
+}
---
+++ ReduxExtension/IReducer.cs
@@ -0,0 +1,8 @@
+﻿namespace Redux
+{
+    /// <summary>
+    /// Contains reducer methods for the redux store's state
+    /// </summary>
+    /// <typeparam name="T">type of store's state</typeparam>
+    public interface IReducer<in T> where T : new() { }
+}
